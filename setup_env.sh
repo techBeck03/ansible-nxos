fd6235934fe8fd270baa661a32f5d67427ff788d
Brandon Beck <beckbran@gmail.com>
2023-06-27 23:13:09 -0500

    adding sudo back to script

--- setup_env.sh
+++ setup_env.sh
@@ -7,5 +7,5 @@ sudo usermod -aG docker $USER
 sudo chown root:docker /var/run/docker.sock
 sudo chown -R root:docker /var/run/docker
 
-docker run -d --name ctf -v /home/ansible/ctf-nxos-ansible:/ctf:z python tail -f /dev/null
-docker exec -it ctf bash
+sudo docker run -d --name ctf -v /home/ansible/ctf-nxos-ansible:/ctf:z python tail -f /dev/null
+sudo docker exec -it ctf bash
